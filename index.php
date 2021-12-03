4e5e9d3747c0b08b0b94237c13035e62e282e1d5
alexsoyes <contact.alexsoyes@gmail.com>
2021-12-03 14:29:30 +0100

    Ajout fonction vérification de page dans l'URL.

--- index.php
+++ index.php
@@ -27,47 +27,54 @@
 </header>
 
 <main>
-    <?php
-    /**
-     * Ici on cherche à inclure dynamiquement une page
-     * en fonction de la variable page dans l'URL.
-     */
-    $page = $_GET['page'];
-    ?>
+    <?php if (array_key_exists('page', $_GET)): ?>
+        <?php
+        // http:://localhost?page=contact
+        /**
+         * Ici on cherche à inclure dynamiquement une page
+         * en fonction de la variable page dans l'URL.
+         */
+        $page = $_GET['page'];
+        ?>
 
-    <p>Vous avez demandé la page : <?php echo $page; ?></p>
+        <p>Vous avez demandé la page : <?php echo $page; ?></p>
 
-    <!--    Équivalent avec les simples / doubles quotes.-->
-    <?php //echo "<p>Vous avez demandé la page : $page</p>"; ?>
-    <?php //echo '<p>Vous avez demandé la page : $page</p>'; ?>
+        <!--    Équivalent avec les simples / doubles quotes.-->
+        <?php //echo "<p>Vous avez demandé la page : $page</p>"; ?>
+        <?php //echo '<p>Vous avez demandé la page : $page</p>'; ?>
 
-    <?php
+        <?php
 
-    /**
-     * On peut utiliser un if, elseif, else...
-     */
-    //    if ($page === "contact") {
-    //        require './pages/contact.php';
-    //    } elseif ($page === "about") {
-    //        require './pages/about.php';
-    //    } else {
-    //        require './pages/homepage.php';
-    //    }
+        /**
+         * On peut utiliser un if, elseif, else...
+         */
+        //    if ($page === "contact") {
+        //        require './pages/contact.php';
+        //    } elseif ($page === "about") {
+        //        require './pages/about.php';
+        //    } else {
+        //        require './pages/homepage.php';
+        //    }
 
-    /**
-     * Ou un swtich !
-     */
-    switch ($page) {
-        case 'contact':
-            require './pages/contact.php';
-            break;
-        case 'about':
-            require './pages/about.php';
-            break;
-        default:
-            require './pages/homepage.php';
-    }
-    ?>
+        /**
+         * Ou un swtich !
+         */
+        switch ($page) {
+            case 'contact':
+                require './pages/contact.php';
+                break;
+            case 'about':
+                require './pages/about.php';
+                break;
+            default:
+                require './pages/404.php';
+        }
+        ?>
+    <?php else: ?>
+        <?php
+        // http://localhost
+        require './pages/homepage.php'; ?>
+    <?php endif; ?>
 </main>
 
 </body>
