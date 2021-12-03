049cccfb7a5b733d15d84036713b468c682d9c58
alexsoyes <contact.alexsoyes@gmail.com>
2021-12-03 15:27:57 +0100

    Début utilisation des dates.

--- pages/homepage.php
+++ pages/homepage.php
@@ -1,2 +1,11 @@
 <h1>Accueil</h1>
 
+<?php
+
+$date = new \DateTime();
+
+$dateFormatted = $date->format('d/m/Y');
+
+echo "<p>Nous sommes le <strong>$dateFormatted</strong>.</p>";
+
+echo $date->getTimestamp();
