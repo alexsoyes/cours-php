68c9f2bf6f14b863b9715e25f05d9f1d48a92af2
alexsoyes <contact.alexsoyes@gmail.com>
2021-12-03 10:26:29 +0100

    Première fonction PHP.

--- pages/contact.php
+++ pages/contact.php
@@ -1,3 +1,35 @@
 <h1>Formulaire de contact</h1>
 
 <p>Nouvelle demande de contact.</p>
+
+<?php
+
+$name = "Soyer";
+$firstname = "Alex";
+$message = "Bonjour !";
+$age = 14;
+$newsletter = true;
+
+/**
+ * Retourne une chaîne de caractères formatés proprement avec le nom et le prénom de la personne.
+ *
+ * @param string $name le nom de la personne
+ * @param string $firstname le prénom de la personne
+ * @return string prénom + nom en majuscule
+ */
+function format(string $name, string $firstname, bool $isFemale = false): string
+{
+    if ($isFemale) {
+        $civility = 'Madame';
+    } else {
+        $civility = 'Monsieur';
+    }
+
+    return 'Bonjour' . ' ' . $civility . ' ' .  $firstname . ' ' . strtoupper($name) . ',';
+}
+
+$formattedNameAndFirstname = format('Soyer', 'Alex');
+
+echo $formattedNameAndFirstname;
+
+
